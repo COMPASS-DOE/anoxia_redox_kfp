623d6f200ff6636de8cbb3a3fc1ce930ea8190f7
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2023-07-03 12:55:25 -0700

    update targets files

--- _targets.R
+++ _targets.R
@@ -15,17 +15,21 @@ tar_option_set(
 options(clustermq.scheduler = "multicore")
 
 # Run the R scripts in the R/ folder with your custom functions:
-
+source("2-code/0-packages.R")
+#source("2-code/0b-initial_processing.R")
+source("2-code/1-functions_processing.R")
 
 # Replace the target list below with your own:
 list(
-  tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100))
-#   format = "feather" # efficient storage of large data frames # nolint
-  ),
-  tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
-  )
+  tar_target(sample_key_file, "1-data/sample_key.csv", format = "file"),
+  tar_target(sample_key, read.csv(sample_key_file)),
+  tar_target(sample_weights_file, "1-data/sample_weights.csv", format = "file"),
+  tar_target(sample_weights, read.csv(sample_weights_file)),
+  
+  # optode
+  tar_target(optode_map, read_sheet("1eFDvH17jjYRsDIx_J8ep_QzPAOO5LBXiGnFZOoA9fSM") %>% mutate_all(as.character)),
+  tar_target(optode_data, import_optode_data("1-data/optodes")),
+  tar_target(optode_data_processed, process_optode_data(optode_data, optode_map, sample_key))
+  
+  
 )
