e344e13d38cc62e32cec03047494abb288e3a6e3
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2023-10-20 11:11:49 -0700

    orp and ghg graphs

--- 2-code/2-functions_analysis.R
+++ 2-code/2-functions_analysis.R
@@ -84,4 +84,50 @@ make_chemistry_graphs = function(combined_data, sample_key){
 
     
 
+    
+    
+    orp_long = 
+      orp %>% 
+      dplyr::select(sample_label, orp_mV, pH, EC_mScm, DO_mgL) %>% 
+      pivot_longer(-sample_label) %>% 
+      left_join(sample_key) %>% 
+      mutate(value = as.numeric(value)) %>% 
+      reorder_factors() %>%  
+      drop_na()
+    
+    orp_long %>% 
+      ggplot(aes(x = location, y = value, color = timepoint))+
+      geom_point(size = 3, stroke = 1,
+                 position = position_dodge(width = 0.3))+
+      scale_shape_manual(values = c(1, 16))+
+      facet_grid(name~treatment, scales = "free_y")
+    
+    
+    ghg_processed = 
+      ghg %>% 
+      filter(!is.na(treatment)) %>% 
+      reorder_factors() 
+    
+    
+    ghg_processed %>% 
+      ggplot(aes(x = location, y = CO2_post/(CH4_post/1000), color = timepoint))+
+      geom_point(size = 3, stroke = 1,
+                 position = position_dodge(width = 0.3))+
+      scale_shape_manual(values = c(1, 16))+
+      facet_wrap(~treatment)+
+      labs(y = "CO2 ppm / CH4 ppm")
+    
+    ghg_long = 
+      ghg_processed %>% 
+      dplyr::select(sample_label, CO2_post, CH4_post) %>% 
+      pivot_longer(cols = -sample_label) %>% 
+      left_join(sample_key) %>% 
+      reorder_factors()
+    
+    ghg_long %>% 
+      ggplot(aes(x = location, y = value, color = timepoint))+
+      geom_point(size = 3, stroke = 1,
+                 position = position_dodge(width = 0.3))+
+      scale_shape_manual(values = c(1, 16))+
+      facet_grid(name ~ treatment, scales = "free_y")
 }
