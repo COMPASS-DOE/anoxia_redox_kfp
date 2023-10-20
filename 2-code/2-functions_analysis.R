0872261f103977ce482031d97aa6b578957bfa65
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2023-10-20 11:11:35 -0700

    graphs

--- 2-code/2-functions_analysis.R
+++ 2-code/2-functions_analysis.R
@@ -8,27 +8,67 @@
 ######################## ####
 ######################## ####
 
+
+
+
+
 make_chemistry_graphs = function(combined_data, sample_key){
   
   combined_key = 
     combined_data %>% 
-    left_join(sample_key)
+    left_join(sample_key) %>% 
+    reorder_factors()
+  
+  ## all variables
+  combined_key %>% 
+    ggplot(aes(x = location, y = value, shape = treatment, color = timepoint))+
+    geom_point(size = 3, stroke = 1,
+               position = position_dodge(width = 0.3))+
+    scale_shape_manual(values = c(1, 16))+
+    facet_wrap(~analyte , scales = "free_y")
+  
+  combined_key %>% 
+    separate(analyte, sep = "_", into = c("analyte")) %>% 
+    ggplot(aes(x = timepoint, y = value, shape = treatment, color = treatment))+
+    geom_point(size = 3, stroke = 1,
+               position = position_dodge(width = 0.3))+
+    scale_shape_manual(values = c(1, 16))+
+  #  facet_grid(analyte ~ location, scales = "free_y")+
+    facet_wrap(~analyte + location, scales = "free_y")+
+    labs(y = "mg/L")
+  
+  
+  
+  #  gg_iron = 
+  combined_key %>% 
+    filter(analysis == "iron") %>% 
+    ggplot(aes(x = location, y = value, color = timepoint))+
+    geom_point(size = 3,
+               position = position_dodge(width = 0.2))+
+    facet_grid(analyte ~ treatment)
   
   iron = 
     combined_key %>% 
     filter(analysis == "iron") %>% 
     pivot_wider(names_from = "analyte", values_from = "value") %>% 
-    mutate(Fe2_3 = Fe2_ppm/Fe3_ppm)
+    mutate(Fe23 = Fe2_ppm/Fe3_ppm)
   
-#  gg_iron = 
-    iron %>% 
-    ggplot(aes(x = location, y = Fe2_3, color = timepoint))+
-    geom_point(position = position_dodge(width = 0.2))+
-      facet_wrap(~treatment)
+  iron %>% 
+    ggplot(aes(x = location, y = Fe23, color = timepoint))+
+    geom_point(size = 3,
+               position = position_dodge(width = 0.2))+
+    facet_grid(. ~ treatment)+
+    labs(y = "Fe2/Fe3")
+
     
-    
-    
-    nitrogen = 
+  combined_key %>% 
+    filter(analyte %in% c("ammonium_ppm", "nitrate_ppm")) %>% 
+    ggplot(aes(x = location, y = value, color = timepoint))+
+    geom_point(size = 3,
+               position = position_dodge(width = 0.2))+
+    facet_grid(analyte ~ treatment, scales = "free_y")
+  
+  nitrogen = 
       combined_key %>% 
       filter(analyte %in% c("ammonium_ppm", "nitrate_ppm")) %>% 
       pivot_wider(names_from = "analyte", values_from = "value") %>% 
@@ -37,18 +77,11 @@ make_chemistry_graphs = function(combined_data, sample_key){
     
     nitrogen %>% 
       ggplot(aes(x = location, y = amm_nitr, color = timepoint))+
-      geom_point(position = position_dodge(width = 0.2))+
-      facet_wrap(~treatment)
+      geom_point(size = 3,
+                 position = position_dodge(width = 0.2))+
+      facet_wrap(~treatment)+
+      labs(y = "ammonium/nitrate")
 
     
-    combined_key %>% 
-      ggplot(aes(x = location, y = value, color = timepoint, shape = treatment))+
-      geom_point(size = 2, position = position_dodge(width = 0.2))+
-      scale_shape_manual(values = c(1, 16))+
-      facet_wrap(analyte ~ treatment, scales = "free_y")
-    
-    
-    
-    
-    
-    }
+
+}
